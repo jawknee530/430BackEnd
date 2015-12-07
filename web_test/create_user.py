fb4579ee9a4ff5a73cc22df766dc4e1cf70a6b4e
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-12-07 11:35:22 -0800

    added some comments

--- web_test/create_user.py
+++ web_test/create_user.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 import json, requests
+
+#ip of the server and route to be tested
 ip = "http://52.27.209.129/"
 route = "create_user.php"
 url = ip+route
 
+#known good response from server to compare to
 testString = "{\"username\":\"jon\",\"email\":\"jawknee530@gmail.com\",\"password\":\"topjam\"}"
 
+#data to be sent to server
 dat = {'username':'jon', 'email':'jawknee530@gmail.com', 'password':'topjam'}
-print "-"*15+"Testing "+"create_user.php"+"-"*15
+#requests.get puts server response into req
 req = requests.get(url, data=json.dumps(dat))
+
+#test output to terminal
+print "-"*15+"Testing "+"create_user.php"+"-"*15
 print testString
 print req.text
+#assert will stop the test if failed
 assert req.text == testString
 print "^^^-----^^^-- Should be user object --^^^-----^^^"
--- web_test/get_user.py
+++ web_test/get_user.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 import json, requests
+
+#ip of the server and route to be tested
 ip = "http://52.27.209.129/"
 route = "get_user.php"
 url = ip+route
 
+#known good response from server ot compare to
 testString = "[{\"username\":\"jon\",\"email\":\"jawknee530@gmail.com\",\"password\":\"topjam\"}]"
 
+#data to be sent to server
 dat = {'username':'jon', 'password':'topjam'}
-print "-"*15+"Testing "+"get_user.php"+"-"*15
+#requests.get puts server response into req
 req = requests.get(url, data=json.dumps(dat))
+
+#print output to terminal
+print "-"*15+"Testing "+"get_user.php"+"-"*15
 print testString
 print req.text
+#assert will stop the test if failed
 assert req.text == testString
 print "^^^-----^^^-- Should be user object --^^^-----^^^"
 
