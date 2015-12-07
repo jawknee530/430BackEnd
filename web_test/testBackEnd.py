377c291acc024e7210fdd7ef87a4b8ef95ac2c97
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-12-07 11:11:20 -0800

    added testing functionality

--- web_test/testBackEnd.py
+++ web_test/testBackEnd.py
@@ -1,23 +1,48 @@
-#!/user/bin/env python
+#!/usr/bin/env python
+import sys
 
-print '*'*70+'\n'
-print '-'*52
-execfile("create_user.py")
-print '-'*52
-execfile("get_user.py")
-print '-'*52
-execfile("build_room.py")
-print '-'*52
-execfile("get_lobby.py")
-print '-'*52
-execfile("add_songs.py")
-print '-'*52
-execfile("delete_song.py")
-print '-'*52
-execfile("delete_room.py")
-print '-'*52
-execfile("remove_user.py")
-print '-'*52
+arg = sys.argv[1]
+if arg == 'a':
+    print '*'*70+'\n'
+    print '-'*52
+    execfile("create_user.py")
+    print '-'*52
+    execfile("get_user.py")
+    print '-'*52
+    execfile("build_room.py")
+    print '-'*52
+    execfile("get_lobby.py")
+    print '-'*52
+    execfile("add_songs.py")
+    print '-'*52
+    execfile("delete_song.py")
+    print '-'*52
+    execfile("delete_room.py")
+    print '-'*52
+    execfile("remove_user.py")
+    print '-'*52
+elif arg == 'c':
+    print '*'*70+'\n'
+    print '-'*52
+    execfile("create_user.py")
+    print '-'*52
+    execfile("get_user.py")
+    print '-'*52
+    execfile("build_room.py")
+    print '-'*52
+    execfile("get_lobby.py")
+    print '-'*52
+    execfile("add_songs.py")
+    print '-'*52
+elif arg == 'd':
+    print '*'*70+'\n'
+    print '-'*52
+    execfile("delete_song.py")
+    print '-'*52
+    execfile("delete_room.py")
+    print '-'*52
+    execfile("remove_user.py")
+    print '-'*52
 
 print '-'*70
 print '-'*27+"All ROUTES PASS"+'-'*28
